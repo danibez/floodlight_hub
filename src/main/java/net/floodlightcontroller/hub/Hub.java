4b03bfbced7f1160fba5670963fac3997e0218ee
Daniel Bezerra <daniel.bezerra92@gmail.com>
2018-07-13 13:07:49 -0300

    multicast working

--- src/main/java/net/floodlightcontroller/hub/Hub.java
+++ src/main/java/net/floodlightcontroller/hub/Hub.java
@@ -28,13 +28,17 @@ import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.IListener.Command;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Data;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.packet.TCP;
+import net.floodlightcontroller.routing.ForwardingBase;
+import net.floodlightcontroller.routing.IRoutingDecision;
 
 import org.projectfloodlight.openflow.protocol.OFFlowAdd;
 import org.projectfloodlight.openflow.protocol.OFMessage;
@@ -58,11 +62,10 @@ import org.projectfloodlight.openflow.types.VlanVid;
  *
  * @author David Erickson (daviderickson@cs.stanford.edu) - 04/04/10
  */
-public class Hub implements IFloodlightModule, IOFMessageListener {
+public class Hub extends ForwardingBase implements IFloodlightModule, IOFMessageListener {
 	private enum HubType {USE_PACKET_OUT, USE_FLOW_MOD};
 	protected static Logger logger;
     private IFloodlightProviderService floodlightProvider;
-    private int count=0;
 
     /**
      * @param floodlightProvider the floodlightProvider to set
@@ -75,7 +78,8 @@ public class Hub implements IFloodlightModule, IOFMessageListener {
     public String getName() {
         return Hub.class.getPackage().getName();
     }
-
+    
+    @Override
     public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
     	switch (msg.getType()) {
 	    	case PACKET_IN:
@@ -93,8 +97,11 @@ public class Hub implements IFloodlightModule, IOFMessageListener {
 	                
 	                String ipDst = dstIp.toString();
 	                logger.info("ipDst: {}",ipDst);
-	                if ((ipDst.compareTo("10.10.0.3") == 0) || (ipDst.compareTo("10.10.0.4") == 0) || (ipDst.compareTo("10.10.0.5") == 0)) {  
-                		if(ipv4.getProtocol() == IpProtocol.TCP) {
+	                if ((ipDst.compareTo("10.10.0.3") == 0) || (ipDst.compareTo("10.10.0.4") == 0) || (ipDst.compareTo("10.10.0.5") == 0)) {
+	                	if(ipv4.getProtocol() == IpProtocol.ICMP) {
+	                		logger.info("ICMP");
+	                	}
+	                	else if(ipv4.getProtocol() == IpProtocol.TCP) {
 		                    /* We got a TCP packet; get the payload from IPv4 */
 		                    TCP tcp = (TCP) ipv4.getPayload();
 		      
@@ -102,7 +109,6 @@ public class Hub implements IFloodlightModule, IOFMessageListener {
 		                    TransportPort srcPort = tcp.getSourcePort();
 		                    TransportPort dstPort = tcp.getDestinationPort();
 	//	                    short flags = tcp.getFlags();
-		                    logger.info("PORT: {} ",dstPort.getPort());
 		                    OFMessage outMessage;
 		                	OFMessage outMessage2;
 		                	OFMessage outMessage3;
@@ -115,16 +121,19 @@ public class Hub implements IFloodlightModule, IOFMessageListener {
 		            	            break;
 		            	        default:
 		            	    	case USE_PACKET_OUT:
-		            	    		MacAddress dstMac = eth.getDestinationMACAddress();
-		            	            outMessage = createHubPacketOut(sw, msg, 3, dstMac);
-		            	            outMessage2 = createHubPacketOut(sw, msg, 4, dstMac);
-		            	            outMessage3 = createHubPacketOut(sw, msg, 5, dstMac);
+		            	    		
+		            	            createHubPacketOut(sw, msg, 3, eth, ipv4);
+		            	            createHubPacketOut(sw, msg, 4, eth, ipv4);
+		            	            createHubPacketOut(sw, msg, 5, eth, ipv4);
 		            	            break;
 		                	}
-		                    sw.write(outMessage);
-		                    sw.write(outMessage2);
-		                    sw.write(outMessage3);
+//		                    sw.write(outMessage);
+//		                    sw.write(outMessage2);
+//		                    sw.write(outMessage3);
                 		}
+	                	else {
+	                		logger.info("ARP");
+	                	}
 	                }
 //	                else
 //	                {
@@ -183,28 +192,74 @@ public class Hub implements IFloodlightModule, IOFMessageListener {
         return fmb.build();
     }
     
-    private OFMessage createHubPacketOut(IOFSwitch sw, OFMessage msg, int port, MacAddress dstMac) {
+    private void createHubPacketOut(IOFSwitch sw, OFMessage msg, int port, Ethernet eth, IPv4 ipv4) {
+    	MacAddress dstMac = MacAddress.of("00:00:00:00:00:0"+ String.valueOf(port) );
+    	MacAddress srcMac = eth.getSourceMACAddress();
+    	Ethernet l2 = new Ethernet();
+    	l2.setSourceMACAddress(srcMac);
+    	l2.setDestinationMACAddress(dstMac);
+    	l2.setEtherType(EthType.IPv4);
+    	
+    	IPv4Address dstIp = IPv4Address.of(10, 10, 0, port);
+    	IPv4Address srcIp = ipv4.getSourceAddress();
+    	IPv4 l3 = new IPv4();
+    	l3.setSourceAddress(srcIp);
+    	l3.setDestinationAddress(dstIp);
+    	l3.setTtl((byte) 64);
+    	l3.setProtocol(IpProtocol.TCP);
+    	
+    	TCP tcp = (TCP) ipv4.getPayload();
+	      
+        /* Various getters and setters are exposed in TCP */
+        TransportPort srcPort = tcp.getSourcePort();
+        TransportPort dstPort = tcp.getDestinationPort();
+    	TCP l4 = new TCP();
+    	l4.setSourcePort(srcPort);
+    	l4.setDestinationPort(dstPort);
+    	
+    	Data l7 = new Data();
+    	
     	OFPacketIn pi = (OFPacketIn) msg;
-        OFPacketOut.Builder pob = sw.getOFFactory().buildPacketOut();
-        pob.setBufferId(pi.getBufferId()).setXid(pi.getXid()).setInPort((pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort() : pi.getMatch().get(MatchField.IN_PORT)));
-        
-        // set actions
-        OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
-        if(port == 0) {
-        	actionBuilder.setPort(OFPort.FLOOD);
-        }
-	    else {
-	        OFPort outPort = OFPort.of(port);
-	        actionBuilder.setPort(outPort);
-	    }
-        pob.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
-
-        // set data if it is included in the packetin
-        if (pi.getBufferId() == OFBufferId.NO_BUFFER) {
+    	if (pi.getBufferId() == OFBufferId.NO_BUFFER) {
             byte[] packetData = pi.getData();
-            pob.setData(packetData);
+            l7.setData(packetData);
         }
-        return pob.build();  
+    	
+    	l2.setPayload(l3);
+    	l3.setPayload(l4);
+    	l4.setPayload(l7);
+    	
+    	byte[] serializedData = l2.serialize();
+    	
+    	OFPacketOut po = sw.getOFFactory().buildPacketOut() /* mySwitch is some IOFSwitch object */
+					    .setData(serializedData)
+					    .setActions(Collections.singletonList((OFAction) sw.getOFFactory().actions().output(OFPort.of(port), port)))
+					    .setInPort(OFPort.CONTROLLER)
+					    .build();
+		  
+		sw.write(po);
+    	
+    	
+//        OFPacketOut.Builder pob = sw.getOFFactory().buildPacketOut();
+//        pob.setBufferId(pi.getBufferId()).setXid(pi.getXid()).setInPort((pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort() : pi.getMatch().get(MatchField.IN_PORT)));
+//        
+//        // set actions
+//        OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
+//        if(port == 0) {
+//        	actionBuilder.setPort(OFPort.FLOOD);
+//        }
+//	    else {
+//	        OFPort outPort = OFPort.of(port);
+//	        actionBuilder.setPort(outPort);
+//	    }
+//        pob.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
+//
+//        // set data if it is included in the packetin
+//        if (pi.getBufferId() == OFBufferId.NO_BUFFER) {
+//            byte[] packetData = pi.getData();
+//            pob.setData(packetData);
+//        }
+//        return pob.build();  
     }
 
     @Override
@@ -252,4 +307,11 @@ public class Hub implements IFloodlightModule, IOFMessageListener {
     public void startUp(FloodlightModuleContext context) {
         floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
     }
+
+	@Override
+	public Command processPacketInMessage(IOFSwitch sw, OFPacketIn pi, IRoutingDecision decision,
+			FloodlightContext cntx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
